e3e806bbd7230e2678375e1060f315487695371e
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-10-16 21:23:58 -0400

    Add incredibly basic testing + CI

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+import nox
+
+SUPPORTED_PYTHONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
+
+nox.options.error_on_external_run = True
+
+
+@nox.session(python=SUPPORTED_PYTHONS)
+def tests(session: nox.Session) -> None:
+    session.install(".")
+    session.run("diff-shades", "--version")
+    session.run("diff-shades", "analyze", os.devnull, "-s", "blackbench", "-w", "cache")
+    session.run("diff-shades", "analyze", os.devnull, "-s", "blackbench", "-w", "cache")
