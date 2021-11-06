5b8c1d6ffddeaee0e6eeb36c37813d97d0555dff
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-11-05 22:33:17 -0400

    Vastly improve the smoke tests

--- noxfile.py
+++ noxfile.py
@@ -8,13 +8,20 @@ nox.needs_version = ">=2021.10.1"
 nox.options.error_on_external_run = True
 
 
-@nox.session(python=SUPPORTED_PYTHONS)
-def tests(session: nox.Session) -> None:
+@nox.session(name="smoke-tests", python=SUPPORTED_PYTHONS)
+def smoke_tests(session: nox.Session) -> None:
     session.install(".")
     session.run("diff-shades", "--version")
     session.install("black")
     target = Path(session.create_tmp(), "fake-devnull")
     cache = Path(session.create_tmp(), "cache")
-    base = ["diff-shades", "--force-color", "analyze", str(target)]
-    session.run(*base, "-s", "blackbench", "-w", str(cache))
-    session.run(*base, "-s", "blackbench", "-s", "black", "-w", str(cache))
+    base = ["diff-shades", "--force-color"]
+    session.run(*base, "analyze", str(target), "-s", "blackbench", "-w", str(cache))
+    session.run(
+        *base, "analyze", str(target), "-s", "blackbench", "-s", "black", "-w", str(cache)
+    )
+    session.run(*base, "show", str(target))
+    session.run(*base, "show", str(target), "blackbench:docs/conf.py")
+    session.run(*base, "inspect", str(target), "src", "blackbench:docs/conf.py")
+    session.run(*base, "compare", str(target), str(target), "--check")
+    session.run(*base, "show-failed", str(target), "--check")
