21c6af03355d749fbbf541448b4258ae55cdf772
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-12-12 18:37:25 -0500

    Improve smoke-tests to include newer features

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import nox
 
+THIS_DIR = Path(__file__).parent
+TESTS_DIR = THIS_DIR / "tests"
 SUPPORTED_PYTHONS = ["3.7", "3.8", "3.9", "3.10"]
 
 nox.needs_version = ">=2021.10.1"
@@ -13,15 +15,24 @@ def smoke_tests(session: nox.Session) -> None:
     session.install(".")
     session.run("diff-shades", "--version")
     session.install("black")
-    target = Path(session.create_tmp(), "fake-devnull")
-    cache = Path(session.create_tmp(), "cache")
+    tmp = Path(session.create_tmp())
+    target = str(tmp / "fake-devnull")
+    cache = str(tmp / "cache")
+    failing = str(TESTS_DIR / "failing.json")
+    failing_two = str(TESTS_DIR / "failing-2.json")
+    log = str(tmp / "log.html")
+    short_file = "src/diff_shades/__init__.py"
+
     base = ["diff-shades", "--force-color"]
-    session.run(*base, "analyze", str(target), "-s", "blackbench", "-w", str(cache))
+    session.run(*base, "analyze", target, "-s", "diff-shades", "-w", cache)
+    session.run(*base, "analyze", target, "-s", "diff-shades", "-w", cache, "--", "-l", "100")
+    session.run(*base, "analyze", target, "-s", "diff-shades", "-s", "ptr", "-w", cache)
+    session.run(*base, "show", target)
+    session.run(*base, "show", target, "diff-shades", "noxfile.py")
+    session.run(*base, "show", target, "diff-shades", short_file, "src")
     session.run(
-        *base, "analyze", str(target), "-s", "blackbench", "-s", "ptr", "-w", str(cache)
+        *base, "--dump-html", log, "show", target, "diff-shades", short_file, "src", "-q"
     )
-    session.run(*base, "show", str(target))
-    session.run(*base, "show", str(target), "blackbench", "docs/conf.py")
-    session.run(*base, "show", str(target), "blackbench", "docs/conf.py", "src")
-    session.run(*base, "compare", str(target), str(target), "--check")
-    session.run(*base, "show-failed", str(target), "--check")
+    session.run(*base, "compare", target, target, "--check")
+    session.run(*base, "compare", failing, failing_two, "--diff")
+    session.run(*base, "show-failed", target, "--check")
